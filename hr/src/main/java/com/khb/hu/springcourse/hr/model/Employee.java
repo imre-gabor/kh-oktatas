f15c2deba190bf6e6d81b462b2b042a3db74877d
imre-gabor <gabor@aut.bme.hu>
2022-11-02 14:48:43 +0100

    manage schema with liquibase

--- hr/src/main/java/com/khb/hu/springcourse/hr/model/Employee.java
+++ hr/src/main/java/com/khb/hu/springcourse/hr/model/Employee.java
@@ -2,13 +2,14 @@ package com.khb.hu.springcourse.hr.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
 public class Employee {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String job;
