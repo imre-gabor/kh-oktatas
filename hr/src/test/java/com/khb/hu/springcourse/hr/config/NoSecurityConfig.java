f64868743e4aff4fc7727eecf21c3c1d7d153e42
imre-gabor <gabor@aut.bme.hu>
2022-11-13 13:05:15 +0100

    fix the NoSecurityConfig class (needed to define PasswordEncoder and AuthenticationManager, because they are injected at applicaiton classes)

--- hr/src/test/java/com/khb/hu/springcourse/hr/config/NoSecurityConfig.java
+++ hr/src/test/java/com/khb/hu/springcourse/hr/config/NoSecurityConfig.java
@@ -4,14 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
+@EnableWebSecurity
 @Profile("nosec")
 public class NoSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        return authManagerBuilder.build();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
